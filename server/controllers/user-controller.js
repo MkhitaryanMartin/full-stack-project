ae0b34d6d989fb86073f4889f3f86655a6d94935
MkhitaryanMartin <1992Martin@mail.ru>
2024-01-08 18:07:42 +0400

    resfreshToken

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -61,7 +61,11 @@ class UserController{
         try {
             const {refreshToken} = req.cookies;
             const userData = await userService.refresh(refreshToken);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, 
+                secure: true, 
+            httpOnly: true,
+            sameSite: 'none'
+            })
             return res.json(userData);
         } catch (e) {
             next(e);
